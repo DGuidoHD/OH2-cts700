f2376472a3f6c0af5448768fb2622bd1a003ec7a
Guido <34964385+DGuidoHD@users.noreply.github.com>
2018-02-01 20:38:12 +0100

    Add files via upload

---
+++ divide10.js
@@ -0,0 +1,8 @@
+// part of git repository OH2-cts700 for Nilan Compact P heat pumps with cts700 touch control
+// copied from Modbus binding 
+
+// Wrap everything in a function
+(function(i) {
+    return parseFloat(i) / 10;
+})(input)
+// input variable contains data passed by openhab
